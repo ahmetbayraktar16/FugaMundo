8ca791261c2e94d3e67841be5e1125b7fcd244ce
Ahmet Bayraktar <apron4x@gmail.com>
2024-01-04 20:20:02 +0300

    Adjust flip player sprite and animation code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    bool playerHasHorizontalSpeed;
     [SerializeField] float moveSpeed = 5f;
+    Animator anim;
     Vector2 moveInput;
     Rigidbody2D rb;
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 
     private void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
         rb.velocity = playerVelocity;
+        anim.SetBool("isRunning", playerHasHorizontalSpeed);
     }
 
     void OnMove(InputValue value)
